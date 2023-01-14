68d94128be1d26e035e103867c158442a25132eb
Zach Leone <zjleone@gmail.com>
2023-01-14 13:29:50 -0700

    Wrote the JavaScript to generate the password and display it

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# password-generator
+# Password Generator
+
+This application is built to generate a random password based on the criteria entered.
+
+## Deployed Application Link
+
--- script.js
+++ script.js
@@ -1,5 +1,59 @@
-// Assignment Code
+// Declaring Variables
 var generateBtn = document.querySelector("#generate");
+var passLength;
+var passLower;
+var passUpper;
+var passNumeric;
+var passSpecial;
+var availCharacterSet = "";
+
+
+//Password Criteria and Prompts
+function criteriaPrompts() {
+  
+  //Get user input for password criteria
+  passLength = prompt("Please select the length of the password between 8 and 128 characters: ");
+  
+  //Alerts user if password length is not valid and ends the function
+  if (passLength < 8 || passLength > 128) {
+    alert("Invalid Password Length");
+    return;
+  }
+
+  passLower = confirm("Would you like to include lowercase characters?");
+  passUpper = confirm("Would you like to include uppercase characters?");
+  passNumeric = confirm("Would you like to include numeric characters?");
+  passSpecial = confirm("Would you like to include special characters?");
+
+  //Create Available Character Set based on the Password criteria
+  if (passLower) {
+    availCharacterSet += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (passUpper) {
+    availCharacterSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (passNumeric) {
+    availCharacterSet += "0123456789";
+  } 
+  if (passSpecial) {
+    availCharacterSet += "!@#$%^&*()";
+  } 
+
+  //Calls the writePassword function once this is complete
+  writePassword();
+}
+
+// Generate the Password
+function generatePassword() {
+  var password = "";
+
+  for (var i = 0; i < passLength; i++) {
+    password += availCharacterSet.charAt(Math.floor(Math.random() * availCharacterSet.length));
+  }
+
+  return password;
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +61,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", criteriaPrompts);
+
+
